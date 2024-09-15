08cb622c552c8870c957edd1cc0d0d031e95908b
hussnain221 <muhammadhusnain9191@gmail.com>
2024-09-15 00:53:43 -0700

    Add files via upload

---
+++ eleven.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("color 0F");
+cout<<"         *     *     *                 " <<endl;
+cout<<"           *     *     *               " <<endl;
+cout<<"            *     *     *              " <<endl;
+cout<<"           *     *     *               " <<endl;
+cout<<"          *     *     *                " <<endl;
+cout<<"           *     *     *               " <<endl;
+cout<<"             *     *     *             " <<endl;
+cout<<" ************************************  " <<endl;
+cout<<"  *                                *   " <<endl;
+cout<<"   *                              *    " <<endl;
+cout<<"     *                          *      " <<endl;
+cout<<"       *                      *        " <<endl;
+cout<<"        **********************         " <<endl;
+cout<< endl;
+cout<<" #WELCOME TO F()():)Y APPLICATION#     " <<endl;
+}
---
+++ ten.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+main()
+{
+system("color 03");
+cout<<" +--^----------,---------,-----,--------^-,  " <<endl;
+cout<<"  | |||||||||   '---------'     |          O " <<endl;
+cout<<"  '+----------------------------^----------| " <<endl;
+cout<<"    ',---------,---------,---------------'   " <<endl;
+cout<<"      / XXXXXX /'|       /'                  " <<endl;
+cout<<"     / XXXXXX /  |      /'                   " <<endl;
+cout<<"    / XXXXXX /'--------'                     " <<endl;
+cout<<"   / XXXXXX /                                " <<endl;
+cout<<"  / XXXXXX /                                 " <<endl;
+cout<<" (_________)                                 " <<endl;
+cout<<"  '-------'                                  " <<endl;
+}
